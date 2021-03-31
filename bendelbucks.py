b51876d0cb2e114ee60d432ea6880c5a5b6c6d74
RayZhengGit <raymond.zheng@mymail.champlain.edu>
2021-03-30 22:54:48 -0400

    finished add_balance function

--- bendelbucks.py
+++ bendelbucks.py
@@ -6,6 +6,7 @@ class BendelBucks:
 
     def __init__(self):
         self.file = "bbdata.txt"
+        self.user_list = ""
 
     def create_user(self, user_id):
         with open(self.file, 'r') as f:
@@ -18,60 +19,83 @@ class BendelBucks:
                 line_id = f.readline()
 
         with open(self.file, 'a') as f:
+            # User ID, Balance, Hourly, Daily, Weekly, endline
             f.write(f'{user_id},0,None,None,None,end\n')
 
-    def add_balance(self, user_id, to_add, method):
+    def add_balance(self, user_id, to_add, *method):
         return_string = ""
         with open(self.file, 'r+') as f:
+            # Line containing the user id
             found_line = False
+            # Has the allowed time passed
             do_write = False
+            # Byte count of the start of the line of the current line
             line_byte = f.tell()
-            line_id = f.readline()
-            while not line_id:
-                split_line = line_id.split('\n', 1)
+            # Line containing data of the user
+            line_data = f.readline()
+            while line_data:
+                # List [User ID, Balance, Hourly, Daily, Weekly, endline]
+                split_line = line_data.split(',')
                 stored_id = split_line[0]
                 if str(stored_id) == str(user_id):
+                    self.user_list = split_line
                     found_line = True
                     break
-                line_id = f.readline()
+                line_data = f.readline()
                 line_byte = f.tell()
-            if not found_line:
-                return
-            if method == "Hourly":
-                current_time = datetime.now()
-                if split_line[2] == "None":
-                    current_bal = int(split_line[1])
-                    current_bal += constants.DAILY_REWARD
-                    split_line[1] = str(current_bal)
-                    split_line[2] = str(current_time)
-                    return_string = f"added {constants.DAILY_REWARD}"
-                    do_write = True
-                else:
-                    last_called = datetime.fromisoformat(split_line[2])
-                    time_difference = current_time - last_called
-                    if time_difference > constants.HOURLY_TIMEOUT:
-                        current_bal = int(split_line[1])
-                        current_bal += constants.DAILY_REWARD
-                        split_line[1] = str(current_bal)
-                        split_line[2] = str(current_time)
-                        return_string = f"added {constants.DAILY_REWARD}"
-                        do_write = True
-                    else:
-                        remaining_time = constants.HOURLY_TIMEOUT -\
-                                         time_difference
-                        return_string = f"must wait \
-                        {round(remaining_time)}s"
+                if not found_line:
+                    return
+            stored_balance = int(self.user_list[1])
+            stored_balance += to_add
+            self.user_list[1] = str(stored_balance)
+
+            do_write = True
+
+            # if method == "Hourly":
+            #     current_time = datetime.now()
+            #     if split_line[2] == "None":
+            #         current_bal = int(split_line[1])
+            #         current_bal += constants.DAILY_REWARD
+            #         split_line[1] = str(current_bal)
+            #         split_line[2] = str(current_time)
+            #         return_string = f"added {constants.DAILY_REWARD}"
+            #         do_write = True
+            #     else:
+            #         last_called = datetime.fromisoformat(split_line[2])
+            #         time_difference = current_time - last_called
+            #         if time_difference > constants.HOURLY_TIMEOUT:
+            #             current_bal = int(split_line[1])
+            #             current_bal += constants.DAILY_REWARD
+            #             split_line[1] = str(current_bal)
+            #             split_line[2] = str(current_time)
+            #             return_string = f"added {constants.DAILY_REWARD}"
+            #             do_write = True
+            #         else:
+            #             remaining_time = constants.HOURLY_TIMEOUT -\
+            #                              time_difference
+            #             return_string = f"must wait \
+            #             {round(remaining_time)}s"
+
             if do_write:
                 line = ""
-                for i in split_line:
+                for i in self.user_list:
                     line += f"{i},"
                 line = line[:-1]
                 f.seek(line_byte)
-                f.write()
+                f.write(line)
             return return_string
 
-# with open("bbdata.txt", 'r+') as f:
-#     print(f.readline())
-#     print(f.tell())
-#     f.seek(41)
-#     f.write("hello")
+    def edit_time_log(self, file_bytes, time_command):
+        return
+
+
+with open("bbdata.txt", 'r+') as f:
+    print(f.readline())
+    f.write("hello")
+
+bb = BendelBucks()
+
+bb.add_balance(819039754611326976, 100)
+
+with open("bbdata.txt", 'r+') as f:
+    print(f.readline())
